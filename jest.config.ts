fa3919db9e70d91c1137967de2f0d390a16eb25e
capt-nemo429 <nemo429@protonmail.com>
2022-08-07 21:07:00 -0300

    chore: fix jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -4,11 +4,6 @@ export default {
   coveragePathIgnorePatterns: ["/node_modules/", "./src/types"],
   coverageProvider: "v8",
   coverageReporters: ["text", "lcov"],
-  coverageThreshold: {
-    global: {
-      lines: 100
-    }
-  },
   preset: "ts-jest",
   roots: ["./src"]
 };
