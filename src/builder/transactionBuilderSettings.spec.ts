5dc19bd923b97002516a4c411437ee692b237297
capt-nemo429 <nemo429@protonmail.com>
2022-08-08 10:06:54 -0300

    test: add unit tests for transactionBuilderSettings

---
+++ src/builder/transactionBuilderSettings.spec.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-magic-numbers */
+import { TransactionBuilderSettings } from "./transactionBuilderSettings";
+
+describe("Transaction builder settings", () => {
+  it("Should hold defaults on constructing", () => {
+    const settings = new TransactionBuilderSettings();
+    expect(settings.canBurnTokens).toEqual(false);
+    expect(settings.maxDistinctTokensPerChangeBox).toEqual(100);
+  });
+
+  it("Should reflect changes", () => {
+    const settings = new TransactionBuilderSettings()
+      .allowTokenBurn(true)
+      .setMaxDistinctTokensPerChangeBox(50);
+
+    expect(settings.canBurnTokens).toEqual(true);
+    expect(settings.maxDistinctTokensPerChangeBox).toEqual(50);
+  });
+});
