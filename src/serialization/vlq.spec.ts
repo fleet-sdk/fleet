bfcf818818a599e4d9835a0858c2ee4d409e94bb
capt-nemo429 <nemo429@protonmail.com>
2022-08-20 14:31:59 -0300

    feat(serializer): add VLQ encoding

---
+++ src/serialization/vlq.spec.ts
@@ -0,0 +1,44 @@
+import { VLQ } from "./vlq";
+
+describe("VLQ serialization - Variable-length quantity", () => {
+  const wikipediaExamples = [
+    { int: 0, bytes: "00" },
+    { int: 127, bytes: "7f" },
+    { int: 128, bytes: "8100" },
+    { int: 8192, bytes: "c000" },
+    { int: 16383, bytes: "ff7f" },
+    { int: 16384, bytes: "818000" },
+    { int: 2097151, bytes: "ffff7f" },
+    { int: 2097152, bytes: "81808000" },
+    { int: 134217728, bytes: "c0808000" },
+    { int: 268435455, bytes: "ffffff7f" }
+  ];
+
+  it("Should encode", () => {
+    wikipediaExamples.forEach((tv) => {
+      expect(VLQ.encode(tv.int).toString("hex")).toEqual(tv.bytes);
+    });
+  });
+
+  it("Should fail trying to encode values over the max value", () => {
+    expect(() => {
+      VLQ.encode(VLQ.maxIntegerValue + 1);
+    }).toThrow();
+  });
+
+  it("Should fail trying to encode negative values", () => {
+    expect(() => {
+      VLQ.encode(-1);
+    }).toThrow();
+  });
+
+  it("Should decode", () => {
+    wikipediaExamples.forEach((tv) => {
+      expect(VLQ.decode(Buffer.from(tv.bytes, "hex"))).toEqual(tv.int);
+    });
+  });
+
+  it("Should decode empty Buffer to 0", () => {
+    expect(VLQ.decode(Buffer.from([]))).toEqual(0);
+  });
+});
---
+++ src/serialization/vlq.ts
@@ -0,0 +1,49 @@
+import { isEmpty } from "../utils/arrayUtils";
+
+const RADIX = 7;
+const MASK = 2 ** RADIX - 1;
+const MAX_VLQ_VALUE = 2147483647;
+
+/**
+ * A **variable-length quantity (VLQ)** is a universal code that uses an arbitrary number
+ * of binary octets (eight-bit bytes) to represent an arbitrarily large integer. A VLQ
+ * is essentially a base-128 representation of an unsigned integer with the addition of
+ * the eighth bit to mark continuation of bytes. VLQ is identical to LEB128 except in
+ * endianness. See the example below.
+ */
+export class VLQ {
+  public static maxIntegerValue = MAX_VLQ_VALUE;
+
+  /**
+   * Encode a unsigned integer to VLQ bytes
+   * @param value unsigned integer
+   * @returns VQL bytes
+   */
+  public static encode(value: number): Buffer {
+    // source: https://rosettacode.org/wiki/Variable-length_quantity#JavaScript
+
+    if (value === 0) {
+      return Buffer.from([value]);
+    } else if (value > MAX_VLQ_VALUE) {
+      throw new RangeError(`Variable Length Quantity not supported for numbers > ${MAX_VLQ_VALUE}`);
+    } else if (value < 0) {
+      throw new RangeError("Variable Length Quantity not supported for negative numbers");
+    }
+
+    const octets: number[] = [];
+    for (let i = value; i !== 0; i >>>= RADIX) {
+      octets.push((i & MASK) + (isEmpty(octets) ? 0 : MASK + 1));
+    }
+
+    return Buffer.from(octets.reverse());
+  }
+
+  /**
+   * Decode VLQ bytes to an unsigned integer value
+   * @param octets VLQ bytes
+   * @returns Unsigned integer value
+   */
+  public static decode(octets: Buffer): number {
+    return octets.reduce((n, octet) => (n << RADIX) + (octet & MASK), 0);
+  }
+}
