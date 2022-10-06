c72afd9c88fcd99e21dd6e0b597d704b2e685022
capt-nemo429 <nemo429@protonmail.com>
2022-10-06 20:05:36 -0300

    feat(bigint utils): add conditional `sumBy()`

--- src/utils/bigIntUtils.spec.ts
+++ src/utils/bigIntUtils.spec.ts
@@ -13,23 +13,57 @@ describe("BigInt toBigInt() conversion", () => {
 });
 
 describe("BigInt sumBy()", () => {
+  const values = [
+    { key: 1, value: 10n },
+    { key: 2, value: 12313n },
+    { key: 3, value: 45n },
+    { key: 4, value: 435345n },
+    { key: 5, value: 3545n },
+    { key: 6, value: 659n },
+    { key: 7, value: 9558n }
+  ];
+  // 448317n
   it("Should sum filled array", () => {
-    const values = [
-      { value: 10n },
-      { value: 12313n },
-      { value: 45n },
-      { value: 435345n },
-      { value: 3545n },
-      { value: 659n },
-      { value: 9558n }
-    ];
-
-    expect(sumBy(values, (x) => x.value)).toEqual(461475n);
+    expect(sumBy(values, (x) => x.value)).toBe(461475n);
   });
 
   it("Should return zero if the array is empty", () => {
     const values: { value: bigint }[] = [];
 
-    expect(sumBy(values, (x) => x.value)).toEqual(0n);
+    expect(sumBy(values, (x) => x.value)).toBe(0n);
+  });
+
+  it("Should sum conditionally", () => {
+    expect(
+      sumBy(
+        values,
+        (x) => x.value,
+        (x) => x.key > 0
+      )
+    ).toBe(461475n);
+
+    expect(
+      sumBy(
+        values,
+        (x) => x.value,
+        (x) => x.key < 0
+      )
+    ).toBe(0n);
+
+    expect(
+      sumBy(
+        values,
+        (x) => x.value,
+        (x) => x.key % 2 === 0
+      )
+    ).toBe(448317n);
+
+    expect(
+      sumBy(
+        values,
+        (x) => x.value,
+        (x) => x.key % 2 !== 0
+      )
+    ).toBe(13158n);
   });
 });
--- src/utils/bigIntUtils.ts
+++ src/utils/bigIntUtils.ts
@@ -1,17 +1,24 @@
+import { isUndefined } from "lodash";
 import { isEmpty } from "./arrayUtils";
 
 export function toBigInt(number: string | number | bigint | boolean): bigint {
   return typeof number === "bigint" ? number : BigInt(number);
 }
 
-export function sumBy<T>(collection: T[], iteratee: (value: T) => bigint): bigint {
+export function sumBy<T>(
+  collection: T[],
+  iteratee: (value: T) => bigint,
+  condition?: (value: T) => boolean
+): bigint {
   let acc = 0n;
   if (isEmpty(collection)) {
     return acc;
   }
 
   for (const item of collection) {
-    acc += iteratee(item);
+    if (isUndefined(condition) || condition(item)) {
+      acc += iteratee(item);
+    }
   }
 
   return acc;
