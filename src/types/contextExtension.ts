c0afa4cb953ff2cdcedea24dc917631e91bbf761
capt-nemo429 <nemo429@protonmail.com>
2022-10-13 20:07:39 -0300

    fix: fix context extension type

--- src/types/contextExtension.ts
+++ src/types/contextExtension.ts
@@ -1,3 +1,3 @@
 import { HexString } from "./common";
 
-export type ContextExtension = { values: { [key: string]: HexString } } | object;
+export type ContextExtension = { values?: { [key: string]: HexString } };
