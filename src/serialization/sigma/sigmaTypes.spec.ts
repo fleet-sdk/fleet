e0691ac680061391211933c6a28d0fb9f9117d29
capt-nemo429 <nemo429@protonmail.com>
2022-11-07 11:34:01 -0300

    tests: fix coverage

---
+++ src/serialization/sigma/sigmaTypes.spec.ts
@@ -0,0 +1,9 @@
+import { SigmaTypeCode } from "./sigmaTypeCode";
+import { SBigInt } from "./sigmaTypes";
+
+describe("Sigma types", () => {
+  it("Should test SBigInt outputs", () => {
+    expect(SBigInt()).toEqual(SigmaTypeCode.BigInt);
+    expect(SBigInt(1n)).toEqual({ value: 1n, type: SigmaTypeCode.BigInt });
+  });
+});
