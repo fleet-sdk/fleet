280308a62d0cd88bc65cb83efd3e1f07754317e2
capt-nemo429 <nemo429@protonmail.com>
2023-06-23 20:17:37 -0300

    common: add `hasKey()` function

--- packages/common/src/utils/objectUtils.ts
+++ packages/common/src/utils/objectUtils.ts
@@ -17,3 +17,7 @@ export function isUndefined(value: unknown): value is undefined {
 export function isDefined<T>(value: T | undefined): value is T {
   return !isUndefined(value);
 }
+
+export function hasKey(o: object, key: PropertyKey) {
+  return Object.prototype.hasOwnProperty.call(o, key);
+}
