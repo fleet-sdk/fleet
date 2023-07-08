5923bbbfd038b29ee8a6cc6756371e1e65584936
capt-nemo429 <nemo429@protonmail.com>
2023-07-08 14:21:51 -0300

    try to fix ci by disabling vitest threads

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    isolate: false,
+    threads: false,
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
