3236dd80c05be6b43108072e326fe05b5e6f7066
arobsn <alissonrobson@proton.me>
2023-07-28 11:40:30 -0300

    common: add new assertion functions

--- packages/common/src/utils/assertions.spec.ts
+++ packages/common/src/utils/assertions.spec.ts
@@ -1,5 +1,13 @@
 import { describe, expect, it, vi } from "vitest";
-import { assert, isEmpty, isFalsy, isTruthy, some } from "./assertions";
+import {
+  assert,
+  assertInstanceOf,
+  assertTypeOf,
+  isEmpty,
+  isFalsy,
+  isTruthy,
+  some
+} from "./assertions";
 
 describe("assert() function", () => {
   const a = 1;
@@ -24,6 +32,65 @@ describe("assert() function", () => {
 
     expect(() => assert(a + b > 3, errorMsgMock.getErrorMsg)).to.throw("error msg test");
     expect(mock).toBeCalledTimes(1);
+  });
+});
+
+describe("assertTypeOf() function", () => {
+  it("Should not throw if condition is met", () => {
+    expect(() => assertTypeOf(429, "number")).not.to.throw;
+    expect(() => assertTypeOf("test", "string")).not.to.throw;
+    expect(() => assertTypeOf({}, "object")).not.to.throw;
+    expect(() => assertTypeOf(1n, "bigint")).not.to.throw;
+  });
+
+  it("Should throw if condition is not met", () => {
+    expect(() => assertTypeOf(429, "string")).to.throw(
+      "Expected an object of type 'string', got 'number'."
+    );
+    expect(() => assertTypeOf("test", "bigint")).to.throw(
+      "Expected an object of type 'bigint', got 'string'."
+    );
+    expect(() => assertTypeOf({}, "function")).to.throw(
+      "Expected an object of type 'function', got 'object'."
+    );
+    expect(() => assertTypeOf(1n, "undefined")).to.throw(
+      "Expected an object of type 'undefined', got 'bigint'."
+    );
+  });
+});
+
+describe("assertInstanceOf() function", () => {
+  it("Should not throw if condition is met", () => {
+    expect(() => {
+      assertInstanceOf(Uint8Array.from([2]), Uint8Array);
+    }).not.to.throw;
+    expect(() => {
+      assertInstanceOf([], Array);
+    }).not.to.throw;
+    expect(() => {
+      assertInstanceOf("", String);
+    }).not.to.throw;
+    expect(() => {
+      assertInstanceOf(() => "", Function);
+    }).not.to.throw;
+  });
+
+  it("Should throw if condition is not met", () => {
+    expect(() => {
+      assertInstanceOf(Uint16Array.from([2]), Uint8Array);
+    }).to.throw("Expected an instance of 'Uint8Array', got 'Uint16Array'");
+    expect(() => {
+      assertInstanceOf([], BigInt);
+    }).to.throw("Expected an instance of 'BigInt', got 'Array'");
+    expect(() => {
+      assertInstanceOf(undefined, Boolean);
+    }).to.throw("Expected an instance of 'Boolean', got 'undefined'");
+    expect(() => {
+      assertInstanceOf(() => true, String);
+    }).to.throw("Expected an instance of 'String', got 'Function'.");
+    expect(() => {
+      assertInstanceOf(null, String);
+    }).to.throw("Expected an instance of 'String', got 'null'.");
   });
 });
 
