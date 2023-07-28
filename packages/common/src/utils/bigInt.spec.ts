24860376673d76c88b56363638e8542bc450aa19
arobsn <alissonrobson@proton.me>
2023-07-28 12:30:14 -0300

    common: move bigIntToHex and hexToBigInt to bytes file

--- packages/common/src/utils/bigInt.spec.ts
+++ packages/common/src/utils/bigInt.spec.ts
@@ -7,10 +7,8 @@ import {
   _1n,
   _63n,
   _7n,
-  bigIntToHex,
   decimalize,
   ensureBigInt,
-  hexToBigInt,
   max,
   min,
   percent,
@@ -184,34 +182,6 @@ describe("BigInt sumBy()", () => {
         (x) => x.key % 2 !== 0
       )
     ).toBe(13158n);
-  });
-
-  it("Should convert a bigint value to a hex string", () => {
-    expect(bigIntToHex(-54417895017443177n)).toBe("ff3eab367a0f9097");
-
-    expect(bigIntToHex(170892133397465074381480318756786823280n)).toBe(
-      "008090a0b0c0d0e0f00010203040506070"
-    );
-    expect(bigIntToHex(-169390233523473389081894288674981388176n)).toBe(
-      "8090a0b0c0d0e0f00010203040506070"
-    );
-
-    expect(bigIntToHex(1207883114728849269100423775319436127n)).toBe(
-      "00e8a13c46cdde58d442c8e45b9f2b5f"
-    );
-    expect(bigIntToHex(-518499127179672366370132270668500813n)).toBe(
-      "9c2404f2634ef40afccc320eed30b3"
-    );
-    expect(bigIntToHex(4n)).toBe("04");
-  });
-
-  it("Should convert a hex string to a bigint value", () => {
-    expect(hexToBigInt("0e8a13c46cdde58d442c8e45b9f2b5f")).toBe(
-      1207883114728849269100423775319436127n
-    );
-    expect(hexToBigInt("9c2404f2634ef40afccc320eed30b3")).toBe(
-      -518499127179672366370132270668500813n
-    );
   });
 });
 
