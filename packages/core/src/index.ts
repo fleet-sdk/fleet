c439ed101c010d5e40f3e98c9ba14ef3463ac949
arobsn <87387688+arobsn@users.noreply.github.com>
2023-08-15 00:21:36 -0300

    fix module exporting

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,4 +2,5 @@ export * from "./builder";
 export * from "./models";
 export * from "./errors";
 export * from "./constantSerializer";
-export { AddressType, Network, Box, Amount, TokenAmount } from "@fleet-sdk/common";
+export { AddressType, Network } from "@fleet-sdk/common";
+export type { Box, Amount, TokenAmount } from "@fleet-sdk/common";
