54a4cb543fa2998a9738bd1fa32604db762248e1
Ross Weir <29697678+ross-weir@users.noreply.github.com>
2023-08-18 09:15:41 +1000

    Add block types to common package (#35)
    * add block types to common package
    
    * remove unneeded quotes

---
+++ packages/common/src/types/block.ts
@@ -0,0 +1,44 @@
+import { HexString } from "./common";
+import { SignedTransaction } from "./transactions";
+
+export type PoWSolution = {
+  pk: HexString;
+  w: HexString;
+  n: string;
+  d: number;
+};
+
+export type BlockHeaderId = string;
+
+export type BlockTransactions = {
+  headerId: BlockHeaderId;
+  transactions: SignedTransaction[];
+};
+
+export type BlockHeader = {
+  id: BlockHeaderId;
+  timestamp: number;
+  version: number;
+  adProofsRoot: HexString;
+  stateRoot: HexString;
+  transactionsRoot: HexString;
+  nBits: number;
+  extensionHash: HexString;
+  powSolutions: PoWSolution;
+  height: number;
+  difficulty: string;
+  parentId: BlockHeaderId;
+  votes: string;
+  size: number;
+  extensionId: HexString;
+  transactionsId: HexString;
+  adProofsId: HexString;
+};
+
+export type Block = {
+  header: BlockHeader;
+  blockTransactions: BlockTransactions;
+  adProofs: unknown; // TODO
+  extension: unknown; // TODO
+  size: number;
+};
