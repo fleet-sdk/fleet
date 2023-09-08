514dcd2c89049f00c257394939cd82e83d33a250
arobsn <87387688+arobsn@users.noreply.github.com>
2023-09-07 23:08:14 -0300

    fix wrong Base16 hash input encoding

--- packages/crypto/src/hashes.spec.ts
+++ packages/crypto/src/hashes.spec.ts
@@ -9,6 +9,14 @@ describe("Hashes smoke tests", () => {
     );
   });
 
+  it("Should have the same result regardless input format", () => {
+    const byte = Uint8Array.from([0xde, 0xad, 0xbe, 0xef]);
+    const hex = "deadbeef";
+
+    expect(blake2b256(byte)).to.be.deep.equal(blake2b256(hex));
+    expect(sha256(byte)).to.be.deep.equal(sha256(hex));
+  });
+
   it("Should hash message using sha256", () => {
     expect(sha256(utf8.decode("sha256"))).to.be.deep.equal(
       hex.decode("5d5b09f6dcb2d53a5fffc60c4ac0d55fabdf556069d6631545f42aa6e3500f2e")
