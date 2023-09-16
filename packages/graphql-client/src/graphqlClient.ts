a1cb88f3e545f2fedf0c20e6a22a619810e234cd
SepehrGanji <sepehrganji79@gmail.com>
2023-09-15 22:38:32 -0600

    basic implementation of gql-client

--- packages/graphql-client/src/graphqlClient.ts
+++ packages/graphql-client/src/graphqlClient.ts
@@ -1,3 +1,5 @@
+import { Header, QueryBoxesArgs } from "@ergo-graphql/types";
+import { Client, createClient, fetchExchange, gql } from "@urql/core";
 import {
   BlockHeader,
   BoxQuery,
@@ -9,25 +11,7 @@ import {
 } from "packages/common/src";
 import { HexString } from "sigmastate-js/main";
 
-export class GraphqlClient implements IChainDataClient<BoxWhere> {
-  getUnspentBoxes(query: BoxQuery<BoxWhere>): Promise<ChainClientBox[]> {
-    throw new Error("Method not implemented.");
-  }
-  getLastHeaders(count: number): Promise<BlockHeader[]> {
-    throw new Error("Method not implemented.");
-  }
-  checkTransaction(transaction: SignedTransaction): Promise<boolean> {
-    throw new Error("Method not implemented.");
-  }
-  submitTransaction(transaction: SignedTransaction): Promise<string> {
-    throw new Error("Method not implemented.");
-  }
-  reduceTransaction(transaction: UnsignedTransaction): Promise<string> {
-    throw new Error("Method not implemented.");
-  }
-}
-
-export type ExtentedBoxWhere = BoxWhere & {
+export type ExtendedBoxWhere = BoxWhere & {
   /** Base16-encoded BoxIds */
   boxIds?: HexString[];
 
@@ -35,4 +19,125 @@ export type ExtentedBoxWhere = BoxWhere & {
   contracts?: HexString[];
 };
 
-export type ExtendedBoxQuery = BoxQuery<ExtentedBoxWhere>;
+export type ExtendedBoxQuery = BoxQuery<ExtendedBoxWhere>;
+
+export class GraphqlClient implements IChainDataClient<BoxWhere> {
+  private _client: Client;
+
+  constructor() {
+    this._client = createClient({
+      url: "TODO",
+      exchanges: [fetchExchange],
+      requestPolicy: "network-only"
+    });
+  }
+  async getUnspentBoxes(q: BoxQuery<ExtendedBoxWhere>): Promise<ChainClientBox[]> {
+    const query = gql<{ boxes: Omit<ChainClientBox, "confirmed">[] }, QueryBoxesArgs>`
+      query boxes(
+        $boxId: String
+        $boxIds: [String!]
+        $contract: String
+        $contracts: [String!]
+        $template: String
+        $tokenId: String
+        $skip: Int
+        $take: Int
+      ) {
+        boxes(
+          boxId: $boxId
+          boxIds: $boxIds
+          ergoTree: $contract
+          ergoTrees: $contracts
+          ergoTreeTemplateHash: $template
+          tokenId: $tokenId
+          skip: $skip
+          take: $take
+        ) {
+          boxId
+          transactionId
+          index
+          value
+          creationHeight
+          ergoTree
+          assets {
+            tokenId
+            amount
+          }
+          additionalRegisters
+        }
+      }
+    `;
+
+    const response = await this._client.query(query, {
+      boxId: q.where.boxId,
+      boxIds: q.where.boxIds,
+      address: q.where.contract,
+      addresses: q.where.contracts,
+      ergoTreeTemplateHash: q.where.template,
+      tokenId: q.where.tokenId,
+      skip: q.skip,
+      take: q.take,
+      spent: false
+    });
+
+    // What to do about confirmed?
+    return response.data?.boxes.map((box) => ({ ...box, confirmed: true })) ?? [];
+  }
+  async getLastHeaders(count: number): Promise<BlockHeader[]> {
+    const query = gql<{ blockHeaders: Header[] }, { count: number }>`
+      query blockHeaders($count: Int) {
+        blockHeaders(take: $count) {
+          headerId
+          timestamp
+          version
+          adProofsRoot
+          stateRoot
+          transactionsRoot
+          nBits
+          extensionHash
+          powSolutions
+          height
+          difficulty
+          parentId
+          votes
+          extension {
+            headerId
+          }
+          adProof {
+            headerId
+          }
+        }
+      }
+    `;
+
+    const response = await this._client.query(query, { count });
+
+    // What is size? Is it the block size?
+    // What is `transactionsId`?
+    return (
+      response.data?.blockHeaders.map((header) => ({
+        ...header,
+        id: header.headerId,
+        timestamp: Number(header.timestamp),
+        nBits: Number(header.nBits),
+        votes: header.votes.join(""),
+        size: 0,
+        transactionsId: "",
+        adProofsId: header.adProof.headerId,
+        extensionId: header.extension.headerId
+      })) ?? []
+    );
+  }
+  checkTransaction(transaction: SignedTransaction): Promise<boolean> {
+    // Size is missing from SignedTransaction! What to do?
+    throw new Error("Method not implemented.");
+  }
+  submitTransaction(transaction: SignedTransaction): Promise<string> {
+    // Same as checkTransaction
+    throw new Error("Method not implemented.");
+  }
+  reduceTransaction(transaction: UnsignedTransaction): Promise<string> {
+    // It's not implemented in graphql. What to do?
+    throw new Error("Method not implemented.");
+  }
+}
