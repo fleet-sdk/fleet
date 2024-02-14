6f4001c6d198a5aa618e9013dc4069306b80b012
arobsn <87387688+arobsn@users.noreply.github.com>
2024-02-14 14:14:03 -0300

    refactor

--- packages/crypto/src/coders/hex.ts
+++ packages/crypto/src/coders/hex.ts
@@ -3,7 +3,7 @@ import { BytesCoder } from "../types";
 
 const HEXES = Array.from({ length: 256 }, (_, i) => i.toString(16).padStart(2, "0"));
 
-const enum HexCharCode {
+const enum HexChar {
   ZERO = 48, // 0
   NINE = 57, // 9
   A_UP = 65, // A
@@ -27,8 +27,9 @@ function hexToBytes(hex: string): Uint8Array {
   assertTypeOf(hex, "string");
   assert(hex.length % 2 === 0, "Invalid hex padding.");
 
-  const bytes = new Uint8Array(hex.length / 2);
-  for (let i = 0, j = 0, len = bytes.length; i < len; i++) {
+  const len = hex.length / 2;
+  const bytes = new Uint8Array(len);
+  for (let i = 0, j = 0; i < len; i++) {
     const n1 = charCodeToBase16(hex.charCodeAt(j++));
     const n2 = charCodeToBase16(hex.charCodeAt(j++));
     bytes[i] = n1 * 16 + n2;
@@ -38,13 +39,9 @@ function hexToBytes(hex: string): Uint8Array {
 }
 
 function charCodeToBase16(char: number) {
-  if (char >= HexCharCode.ZERO && char <= HexCharCode.NINE) {
-    return char - HexCharCode.ZERO;
-  } else if (char >= HexCharCode.A_UP && char <= HexCharCode.F_UP) {
-    return char - (HexCharCode.A_UP - 10);
-  } else if (char >= HexCharCode.A_LO && char <= HexCharCode.F_LO) {
-    return char - (HexCharCode.A_LO - 10);
-  }
+  if (char >= HexChar.ZERO && char <= HexChar.NINE) return char - HexChar.ZERO;
+  if (char >= HexChar.A_UP && char <= HexChar.F_UP) return char - (HexChar.A_UP - 10);
+  if (char >= HexChar.A_LO && char <= HexChar.F_LO) return char - (HexChar.A_LO - 10);
 
   throw new Error("Invalid byte sequence.");
 }
