365d00aff4af7ad6a71ab0e27f8e3b954dc6b3bf
arobsn <87387688+arobsn@users.noreply.github.com>
2024-04-02 20:41:17 -0300

    refactor

--- packages/wallet/src/prover/proveDLogProtocol.ts
+++ packages/wallet/src/prover/proveDLogProtocol.ts
@@ -1,15 +1,36 @@
-import { concatBytes } from "@fleet-sdk/common";
+import { _0n, concatBytes } from "@fleet-sdk/common";
+import type { Coder } from "@fleet-sdk/crypto";
 import { blake2b256, hex, randomBytes } from "@fleet-sdk/crypto";
-import { schnorr, secp256k1 } from "@noble/curves/secp256k1";
+import { secp256k1 } from "@noble/curves/secp256k1";
 
 const { ProjectivePoint: ECPoint, CURVE } = secp256k1;
-const { numberToBytesBE, bytesToNumberBE, mod } = schnorr.utils;
 const G = ECPoint.BASE;
 
 const BLAKE2B_256_DIGEST_LEN = 32;
 const ERGO_SOUNDNESS_BYTES = 24;
 const ERGO_SCHNORR_SIG_LEN = BLAKE2B_256_DIGEST_LEN + ERGO_SOUNDNESS_BYTES;
 const MAX_ITERATIONS = 100;
+
+/**
+ * A coder for Big Endian  `BigInt` <> `Uint8Array` conversion..
+ */
+const bigint: Coder<Uint8Array, bigint> = {
+  /**
+   * Encode a `Uint8Array` to a `BigInt`.
+   */
+  encode(data) {
+    const hexInput = hex.encode(data);
+    return BigInt(hexInput == "" ? "0" : "0x" + hexInput);
+  },
+
+  /**
+   * Decode a `BigInt` to a `Uint8Array`.
+   */
+  decode(data) {
+    const hexData = data.toString(16);
+    return hex.decode(hexData.length % 2 ? "0" + hexData : hexData);
+  }
+};
 
 /**
  * Generates a Schnorr signature for the given message using the provided secret key.
@@ -32,24 +53,24 @@ export function sign(message: Uint8Array, secretKey: Uint8Array) {
  *
  * @param message - The message to be signed.
  * @param secretKey - The secret key used for signing.
- * @returns The generated signature as a Uint8Array, or null if the verification fails.
+ * @returns The generated signature as a Uint8Array, or undefined if the verification fails.
  * @throws Error if failed to generate commitment.
  */
-export function genSignature(message: Uint8Array, secretKey: Uint8Array): null | Uint8Array {
-  const sk = bytesToNumberBE(secretKey);
+export function genSignature(message: Uint8Array, secretKey: Uint8Array): undefined | Uint8Array {
+  const sk = bigint.encode(secretKey);
   const y = genY();
   const w = G.multiply(y).toRawBytes();
   const pk = G.multiply(sk).toRawBytes();
-  const s = concatBytes(genCommitment(pk, w), message);
-  const c = numHash(s);
-  if (c === 0n) throw new Error("Failed to generate commitment");
-  const z = mod(sk * c + y, CURVE.n);
+  const s = genCommitment(pk, w, message);
+  const c = fiatShamirHash(s);
+  if (c === 0n) throw new Error("Failed to generate challenge");
+  const z = umod(sk * c + y, CURVE.n);
 
-  const cb = numberToBytesBE(c, ERGO_SOUNDNESS_BYTES);
-  const zb = numberToBytesBE(z, BLAKE2B_256_DIGEST_LEN);
+  const cb = bigint.decode(c);
+  const zb = bigint.decode(z);
   const signature = concatBytes(cb, zb);
 
-  if (!verify(message, signature, pk)) return null;
+  if (!verify(message, signature, pk)) return;
   return signature;
 }
 
@@ -64,7 +85,7 @@ function genY() {
   let c = 0;
 
   while (y === 0n && c < MAX_ITERATIONS) {
-    y = mod(bytesToNumberBE(randomBytes(32)), CURVE.n);
+    y = umod(bigint.encode(randomBytes(32)), CURVE.n);
     c++;
   }
 
@@ -73,41 +94,48 @@ function genY() {
 }
 
 /**
+ * Calculates the unsigned modulo of two bigint values.
+ * @param a - The dividend.
+ * @param b - The divisor.
+ */
+export function umod(a: bigint, b: bigint): bigint {
+  const result = a % b;
+  return result >= _0n ? result : b + result;
+}
+
+/**
  * Verifies the Schnorr signature for a given message using the provided public key and signature.
  * @param message - The message to be verified.
- * @param signature - The signature to be verified.
+ * @param proof - The proof to be verified.
  * @param publicKey - The public key corresponding to the private key used to generate the signature.
  * @returns A boolean indicating whether the signature is valid or not.
  */
-export function verify(message: Uint8Array, signature: Uint8Array, publicKey: Uint8Array) {
-  if (signature.length !== ERGO_SCHNORR_SIG_LEN) return false;
+export function verify(message: Uint8Array, proof: Uint8Array, publicKey: Uint8Array) {
+  if (proof.length !== ERGO_SCHNORR_SIG_LEN) return false;
 
-  const c = bytesToNumberBE(signature.slice(0, ERGO_SOUNDNESS_BYTES));
-  const z = bytesToNumberBE(signature.slice(ERGO_SOUNDNESS_BYTES, ERGO_SCHNORR_SIG_LEN));
+  const c = bigint.encode(proof.slice(0, ERGO_SOUNDNESS_BYTES));
+  const z = bigint.encode(proof.slice(ERGO_SOUNDNESS_BYTES, ERGO_SCHNORR_SIG_LEN));
 
   const t = ECPoint.fromHex(publicKey).multiply(CURVE.n - c);
   const w = G.multiply(z).add(t).toRawBytes();
-  const s = concatBytes(genCommitment(publicKey, w), message);
+  const s = genCommitment(publicKey, w, message);
 
-  return numHash(s) === c;
+  return fiatShamirHash(s) === c;
 }
 
 /**
- * Computes the numeric hash of a given message.
- *
- * @param message - The message to be hashed.
- * @returns The numeric hash of the message.
+ * Computes the numeric Fiat-Shamir hash of a given message.
  */
-function numHash(message: Uint8Array) {
-  return bytesToNumberBE(blake2b256(message).slice(0, ERGO_SOUNDNESS_BYTES));
+function fiatShamirHash(message: Uint8Array) {
+  return bigint.encode(blake2b256(message).slice(0, ERGO_SOUNDNESS_BYTES));
 }
 
 const COMMITMENT_PREFIX = hex.decode("010027100108cd");
-const COMMITMENT_SUFFIX = hex.decode("73000021");
+const COMMITMENT_POSTFIX = hex.decode("73000021");
 
 /**
- * Compute commitment prefix P(c) = H(prefix || pk || postfix || w)
+ * Generates a commitment for the given public key, w, and message.
  */
-function genCommitment(pk: Uint8Array, w: Uint8Array) {
-  return concatBytes(COMMITMENT_PREFIX, pk, COMMITMENT_SUFFIX, w);
+function genCommitment(pk: Uint8Array, w: Uint8Array, message: Uint8Array) {
+  return concatBytes(COMMITMENT_PREFIX, pk, COMMITMENT_POSTFIX, w, message);
 }
