eb697067c878322de309ee2b35271b16fb7539f8
arobsn <87387688+arobsn@users.noreply.github.com>
2024-06-21 08:39:15 -0300

    export wordlists

---
+++ packages/wallet/src/wordlists.ts
@@ -0,0 +1,10 @@
+export { wordlist as english } from "@scure/bip39/wordlists/english";
+export { wordlist as japanese } from "@scure/bip39/wordlists/japanese";
+export { wordlist as korean } from "@scure/bip39/wordlists/korean";
+export { wordlist as spanish } from "@scure/bip39/wordlists/spanish";
+export { wordlist as chinese_simplified } from "@scure/bip39/wordlists/simplified-chinese";
+export { wordlist as chinese_traditional } from "@scure/bip39/wordlists/traditional-chinese";
+export { wordlist as french } from "@scure/bip39/wordlists/french";
+export { wordlist as italian } from "@scure/bip39/wordlists/italian";
+export { wordlist as portuguese } from "@scure/bip39/wordlists/portuguese";
+export { wordlist as czech } from "@scure/bip39/wordlists/czech";
