3a94a976149915a1d6d6d1bd8a9c3cfa459bcfb5
arobsn <87387688+arobsn@users.noreply.github.com>
2024-09-17 15:22:20 -0300

    refactor

--- plugins/babel-fees/src/utils.ts
+++ plugins/babel-fees/src/utils.ts
@@ -1,6 +1,6 @@
-import { isDefined, isHex, isUndefined } from "@fleet-sdk/common";
+import { isHex } from "@fleet-sdk/common";
 import type { Amount, Box, R4ToR5Registers } from "@fleet-sdk/core";
-import { parse, SLong, SSigmaProp, stypeof } from "@fleet-sdk/serializer";
+import { SConstant, SLong, SSigmaProp, stypeof } from "@fleet-sdk/serializer";
 import { BABEL_ERGOTREE_PREFIX, BABEL_ERGOTREE_SUFFIX } from "./constants";
 
 const TOKEN_ID_HEX_LENGTH = 64;
@@ -9,16 +9,13 @@ const BABEL_CONTRACT_LENGTH =
   BABEL_ERGOTREE_PREFIX.length + TOKEN_ID_HEX_LENGTH + BABEL_ERGOTREE_SUFFIX.length;
 
 /**
- * Get the the price for a token unit in NanoErgs
+ * Get the the price for a token unit in Nanoergs
  * @param babelBox
  * @returns
  */
 export function getTokenPrice(babelBox: Box<Amount>): bigint {
-  if (isUndefined(babelBox.additionalRegisters.R5) || !isValidBabelBox(babelBox)) {
-    throw Error("Invalid babel box.");
-  }
-
-  return parse(babelBox.additionalRegisters.R5);
+  if (!isValidBabelBox(babelBox)) throw Error("Invalid babel box.");
+  return SConstant.from<bigint>(babelBox.additionalRegisters.R5).data;
 }
 
 /**
@@ -27,10 +24,7 @@ export function getTokenPrice(babelBox: Box<Amount>): bigint {
  * @returns returns a Babel Fee ErgoTree
  */
 export function buildBabelContract(tokenId: string): string {
-  if (!isValidTokenId(tokenId)) {
-    throw new Error("Invalid Token ID");
-  }
-
+  if (!isValidTokenId(tokenId)) throw new Error("Invalid Token ID");
   return `${BABEL_ERGOTREE_PREFIX}${tokenId}${BABEL_ERGOTREE_SUFFIX}`;
 }
 
@@ -67,9 +61,7 @@ export function isValidBabelBox(box: Box<Amount>): box is BabelBox {
 }
 
 export function isBabelContractForTokenId(ergoTree: string, tokenId: string) {
-  if (!isValidBabelContract(ergoTree)) {
-    return false;
-  }
+  if (!isValidBabelContract(ergoTree)) return false;
 
   const extractedTokenId = ergoTree.slice(
     BABEL_ERGOTREE_PREFIX.length,
@@ -80,9 +72,7 @@ export function isBabelContractForTokenId(ergoTree: string, tokenId: string) {
 }
 
 export function extractTokenIdFromBabelContract(ergoTree: string): string {
-  if (!isValidBabelContract(ergoTree)) {
-    throw new Error("Invalid Babel Fee contract");
-  }
+  if (!isValidBabelContract(ergoTree)) throw new Error("Invalid Babel Fee contract");
 
   return ergoTree.slice(
     BABEL_ERGOTREE_PREFIX.length,
