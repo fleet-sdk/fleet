62025c47ae9bd4eba24b4b88d89372b678320f99
arobsn <87387688+arobsn@users.noreply.github.com>
2024-09-17 10:49:39 -0300

    Add utility method to test register type Fixes #122

--- packages/serializer/src/index.ts
+++ packages/serializer/src/index.ts
@@ -1,4 +1,4 @@
 export { estimateVLQSize, SigmaByteReader, SigmaByteWriter } from "./coders";
-export { SConstant, parse, decode } from "./sigmaConstant";
+export { SConstant, parse, decode, stypeof } from "./sigmaConstant";
 export * from "./types";
 export * from "./serializers";
