4426d9035fefcf68479d3ec9d4441bf8d815a9c8
arobsn <87387688+arobsn@users.noreply.github.com>
2025-01-23 20:42:39 -0300

    add test vector

--- packages/serializer/src/_test-vectors/constantVectors.ts
+++ packages/serializer/src/_test-vectors/constantVectors.ts
@@ -51,7 +51,8 @@ export const intVectors: ConstantTestVector<number>[] = [
   { hex: "042d", value: -23 },
   { hex: "04800f", value: 960 },
   { hex: "04808008", value: 65536 },
-  { hex: "04808023", value: 286720 }
+  { hex: "04808023", value: 286720 },
+  { hex: "04feffffffffffffffff01", value: 2147483647 }, // int32 max
 ];
 
 export const longVectors: ConstantTestVector<bigint | string>[] = [
