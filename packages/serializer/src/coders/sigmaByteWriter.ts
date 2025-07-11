ad178a625be11270ce8d89fd458e011a6e5c5ae0
arobsn <87387688+arobsn@users.noreply.github.com>
2025-07-10 22:37:19 -0300

    add `writeOption` to `SigmaByteWriter`

--- packages/serializer/src/coders/sigmaByteWriter.ts
+++ packages/serializer/src/coders/sigmaByteWriter.ts
@@ -109,6 +109,21 @@ export class SigmaByteWriter {
     return this;
   }
 
+  writeOption<T>(
+    value: T | undefined,
+    innerWriter: (writer: SigmaByteWriter) => void
+  ): SigmaByteWriter {
+    if (value === undefined) {
+      this.write(0x00); // write 0x00 for None
+      return this;
+    }
+
+    this.write(0x01); // write 0x01 for Some
+    innerWriter(this); // call the inner writer to write the Option value
+
+    return this;
+  }
+
   writeChecksum(length = 4, hashFn = blake2b256): SigmaByteWriter {
     const hash = hashFn(this.toBytes());
     return this.writeBytes(length ? hash.subarray(0, length) : hash);
